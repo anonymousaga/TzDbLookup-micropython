8f78250a44633be11f26ee748d528f3d1ca6bb84
Abhijeet <68388009+anonymousaga@users.noreply.github.com>
2025-06-08 11:21:10 -0400

    Update example.py

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 import tz_data
 
 print(tz_data.lookup("Atlantic/Reykjavik"))       # -> GMT0
-print(tz_data.lookup("atlantic/south_georgia"))   # -> <-02>2
+print(tz_data.lookup("atlantic/south_georgia"))   # -> <-02>2    ### the program is case-insensitive
 print(tz_data.lookup("invalid/zone"))             # -> None
